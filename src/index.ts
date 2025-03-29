b4f5d1a28e23b5a4484a944b8083853fc1f55774
Shaun <shaun.underwood@bamgsports.com>
2025-03-29 09:56:39 +0000

    Service-ify ollama integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { config } from './config';
 import SlackService from './services/slack.service';
 import JiraService from './services/jira.service';
+import OllamaService from './services/ollama.service';
 
 const jiraService = new JiraService(config.jira);
-const slackService = new SlackService(config.slack, jiraService);
+const ollamaService = new OllamaService(config.ollama);
+const slackService = new SlackService(config.slack, jiraService, ollamaService);
 
 // Start the app
 (async () => {
